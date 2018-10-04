78ae0d4b14ac73685771403d6f4b53c528e2f239
Nicholas Smith <nico.ehs05@gmail.com>
2018-10-04 13:28:26 +0000

    automatically backed up by learn

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,17 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @limit=limit
+  end
+  
+  def 
+  
+  def collect_multiples
+    (1..@limit).to_a.select{|n| n%3==0 or n%5==0}
+  end
+
+
+  def sum_multiples
+    collect_multiples(limit).reduce(0, :+)
+  end
+end
